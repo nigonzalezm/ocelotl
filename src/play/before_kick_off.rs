2298b01db306aac8f1bcc706f05e328ac0899183
Nelson González <nelson.gonzalez@nextialab.com>
2022-02-14 23:51:52 -0600

    Most basic behaviour

--- src/play/before_kick_off.rs
+++ src/play/before_kick_off.rs
@@ -1,6 +1,10 @@
+use super::super::base::connect::Connect;
 use super::super::game::localization::Position;
 use super::super::server::see::Ball;
+use std::sync::Arc;
 
-pub fn execute(position: &Position, ball: Option<Ball>) {
-
+pub fn execute(connect: &Arc<Connect>, position: &Position, _opt_ball: Option<Ball>) {
+    if position.distance_to(-10.0, 10.0) > 2.0 {
+        connect.send("(move -10 0)".to_string());
+    }
 }
